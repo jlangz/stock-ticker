a708bb565f4bc92a0c2f30d8e406563cbdc145bd
Jakob Langseth <jakob.langseth@investisdigital.com>
2024-02-05 18:49:33 +0100

    Initial gh-pages commit

--- src/lib/stores/stockStore.ts
+++
@@ -1,6 +0,0 @@
-import { writable } from 'svelte/store';
-import type { Stock } from '../types/Stock';
-
-export const mostMovingStocks = writable<Stock[]>([]);
-export const stockError = writable<string | null>(null);
-export const isLoading = writable(true);
--- src/lib/types/Stock.ts
+++
@@ -1,8 +0,0 @@
-export interface Stock {
-  id: number | string;
-  symbol: string;
-  name: string;
-  change: number;
-  price: number;
-  changesPercentage: number;
-}
